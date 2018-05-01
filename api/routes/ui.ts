b3bbf0c37f2f255983f4bb9f9a742e926bb492f4
Saurabh Bansal <saurabh.bansal@marinsoftware.com>
2018-05-01 08:43:13 -0700

    ui to public

--- api/routes/ui.ts
+++ api/routes/ui.ts
@@ -1,5 +1,5 @@
 module.exports = function(app){
     app.get('/',function(req,res){
-        res.sendfile('index.html', { root: __dirname + "/../../ui" } );
+        res.sendfile('index.html', { root: __dirname + "/../../public" } );
     });
 }
